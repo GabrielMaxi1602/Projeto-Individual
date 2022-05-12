027fd87efcf871dcdf8991cfeb1da2e2b434c44b
Qualquer <Qualquer@gmail.com>
2022-05-12 14:39:10 -0300

    progresso na tela de login e ideias do site

---
+++ css/JS/main.js
@@ -0,0 +1,7 @@
+function mudanca_tela(){
+    var personagens = ['url(../imgs/yoru_login.jpg)', 'url(../imgs/raze.png)', 'url(../imgs/reyna.jpg)']
+
+    var computador = Math.floor(Math.random(1) * (3));
+    
+    document.body
+}
