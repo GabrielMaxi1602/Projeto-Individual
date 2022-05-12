18b969eae18fbe704c8fdb4a03d3164de8c4a07c
Qualquer <Qualquer@gmail.com>
2022-05-12 15:55:57 -0300

    Finalizaçao da tela de login

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,31 @@
 function mudanca_tela(){
-    var personagens = ['url(./imgs/yoru_login.jpg)', 'url(./imgs/raze.png)', 'url(./imgs/skye.jpg)']
+    var personagens = ['url(./imgs/yoru.jpg)', 'url(./imgs/raze.png)', 'url(./imgs/skye.jpg)']
 
+    var cor_borda = ['#00aeff', '#dde909', '#009d03']
+
+    var blor = ['#00ccff 3px 3px 30px 5px','#efbd08 3px 3px 30px 5px','#1d6201 3px 3px 30px 5px']
+    var cor = ['#000ba1','#efbd08','#1d6201']
+    
     var computador = Math.floor(Math.random(1) * (3));
     
+    
     document.body.style.backgroundImage = personagens[computador];
+    caixaPai.style.borderColor = cor_borda[computador];
+    caixaPai.style.boxShadow = blor[computador];
+    botao.style.borderColor = cor_borda[computador];
+    botao.style.boxShadow = blor[computador];
+    botao.style.backgroundColor = cor[computador];
+    input_email.style.borderColor = cor[computador];
+    input_senha.style.borderColor = cor[computador];
+    input_email.style.color = cor[computador];
+    input_senha.style.color = cor[computador];
+
+
+    
+    console.log(personagens[computador])
 }
 
 setInterval(function(){
     mudanca_tela()
-},4000)
+},2000);
+
