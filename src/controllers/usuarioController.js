71f4c27de8246755a14e1071d7ef60ce37470f2f
Gabriel Maximiano <gabriel.mgomes@sptech.school>
2022-05-20 15:16:10 -0300

    Ativação da API na tela de cadastro e login

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -65,6 +65,11 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var usuario = req.body.usuarioServer;
+    var cep = req.body.cepServer;
+    var estado = req.body.estadoServer;
+    var idade = req.body.idadeServer;
+
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -73,10 +78,21 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else {
+    }else if (usuario == undefined) {
+        res.status(400).send("Seu usuario está undefined!");
+    }else if (cep == undefined) {
+        res.status(400).send("Seu cep está undefined!");
+    }else if (estado == undefined) {
+        res.status(400).send("Seu estado está undefined!");
+    }else if (idade == undefined) {
+        res.status(400).send("Sua idade está undefined!");
+    }
+
+    else {
+        
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, cep, estado, idade, usuario)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,77 +1,29 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-/* para workbench - local - desenvolvimento */
-CREATE DATABASE acquatec;
+create database projeto_valorant;
 
-USE acquatec;
+use projeto_valorant;
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+create table skin(
+idSkin int primary key auto_increment,
+nomeSkin varchar(200),
+dano decimal(6,2),
+custo decimal(6,2),
+tipo varchar(200)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table usuario(
+idUsuario int primary key auto_increment,
+nome varchar(200),
+nomeUsuario varchar(200),
+idade int,
+senha varchar(200),
+cep varchar(20),
+estado varchar(20),
+email varchar(200)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
-
-/* altere esta tabela de acordo com o que está em INSERT de sua API do arduino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-
-/* para sql server - remoto - produção */
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300)
-);
-
-/* altere esta tabela de acordo com o que está em INSERT de sua API do arduino */
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
+create table sorteio (
+fk_usuario int,
+foreign key (fk_usuario) references usuario(idUsuario),
+fk_skin int,
+foreign key (fk_skin) references skin(idSkin)
 );
