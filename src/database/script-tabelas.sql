9aa9b0c1c077ded8b679320bbf341275e1abb920
Gabriel Maximiano <gabriel.mgomes@sptech.school>
2022-06-06 03:21:10 -0300

    finalização do projeto e ultimo push antes da apresentação

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -24,42 +24,46 @@ VP int default 200
 );
 
 
-SELECT * FROM SKIN;
+SELECT * FROM usuario;
 
 create table resultado_skin(
 fk_skin int,
 foreign key (fk_skin) references skin(idSkin), 
 fk_usuario int,
-foreign key (fk_usuario) references usuario(idUsuario)
- );
+foreign key (fk_usuario) references usuario(idUsuario),
+sr int primary key auto_increment 
+ )auto_increment = 1000;
 
 select * from usuario;
 
 
+select * from skin;
 select * from resultado_skin;
 
+select * from skin join resultado_skin on idSkin = fk_skin;
+
 insert into skin values 
-(null,'faca_dente_dragao','100','180.00','lendaria','faca'),
-(null,'faca_oni','100','190.00','lendaria','faca'),
-(null,'operator_sentineça','300','110.00','lendaria','operator'),
-(null,'phaton_oni','90','80.00','lendaria','phanton'),
-(null,'phaton_trevas','90','85.00','lendaria','phanton'),
-(null,'vandal_dragao','110','110.00','lendaria','vandal'),
-(null,'ghost_trevas','50','60.00','epica','ghost'),
-(null,'marshall_dourada','130','90.00','epica','marshall'),
-(null,'phaton_glitchpop','90','90.00','epica','phanton'),
-(null,'operator_dourada','250','170.00','epica','operator'),
-(null,'vandal_sentinela','110','100.00','epica','vandal'),
-(null,'ghost_oceano','50','40.00','rara','ghost'),
-(null,'operator_ion','250','70.00','rara','operator'),
-(null,'phanton_ion','90','70.00','rara','phanton'),
-(null,'sheriff_bosque','210','50.00','rara','sheriff'),
-(null,'sheriff_ferrugem','210','30.00','rara','sheriff'),
-(null,'ghost_megapunk','50','20.00','normal','ghost'),
-(null,'marshall_espinhos','130','10.00','normal','marshall'),
-(null,'phanton_reyna','90','10.00','normal','phanton'),
-(null,'sheriff_sakura','120','20.00','normal','sheriff'),
-(null,'vandal_roxa','110','5.00','normal','vandal');
+(null,'ANCIFOGO','100','180.00','lendaria','faca'),
+(null,'ONI','100','190.00','lendaria','faca'),
+(null,'SENTINELA','300','110.00','lendaria','operator'),
+(null,'ONI','90','80.00','lendaria','phanton'),
+(null,'RUÍNA','90','85.00','lendaria','phanton'),
+(null,'ANCIFOGO','110','110.00','lendaria','vandal'),
+(null,'RUÍNA','50','60.00','epica','ghost'),
+(null,'SOBERANIA','130','90.00','epica','marshall'),
+(null,'GLITCHPOP','90','90.00','epica','phanton'),
+(null,'SOBERANIA','250','170.00','epica','operator'),
+(null,'SENTINELA','110','100.00','epica','vandal'),
+(null,'OCEÂNICA','50','40.00','rara','ghost'),
+(null,'ION','250','70.00','rara','operator'),
+(null,'ION','90','70.00','rara','phanton'),
+(null,'BOSQUE','210','50.00','rara','sheriff'),
+(null,'DESOLAÇÃO','210','30.00','rara','sheriff'),
+(null,'MAGEPUNK','50','20.00','normal','ghost'),
+(null,'METALÓDICA','130','10.00','normal','marshall'),
+(null,'GO!REYNA','90','10.00','normal','phanton'),
+(null,'SAKURA','120','20.00','normal','sheriff'),
+(null,'COLMEIA','110','5.00','normal','vandal');
 
 -- insert into resultado_skin values
  
@@ -67,25 +71,47 @@ insert into skin values
  join resultado_skin as rs on s.idSkin = rs.fk_skin group by s.classe;
  
  
- select * from resultado_skin;
- 
-
- 
- 
+ select * from skin;
  select * from usuario;
+ select * from resultado_skin;
  
  select count(fk_skin) as 'quantidade drop
  ' from resultado_skin;
  
- insert into usuario values 
- (null,'gabriel','gamax','18','12345','08450320','SP','valorant.a@gmail.com',null);
+
+ 
  
  select tipo , count(fk_skin) as 'qtd_drop' from skin as sk join resultado_skin as rs on idSkin = fk_skin group by tipo;
  
  update usuario set vp = '4000' where idUsuario = '1';
  
+ select tipo , count(fk_skin) as 'qtd_drop' from skin as sk join resultado_skin as rs on idSkin = fk_skin group by tipo;
+ 
+ -- insert into resultado_skin values 
+ (1,1,null),
+ (2,1,null),
+ (3,1,null),
+ (4,1,null),
+ (5,1,null),
+ (6,1,null),
+ (7,1,null),
+ (8,1,null),
+ (9,1,null),
+ (10,1,null),
+ (11,1,null),
+ (12,1,null),
+ (13,1,null),
+ (14,1,null),
+ (15,1,null),
+ (16,1,null),
+ (17,1,null),
+ (18,1,null),
+ (19,1,null),
+ (20,1,null),
+ (21,1,null);
+ 
+ -- truncate table resultado_skin;
  
  select idSkin, nomeSkin , dano , custo , tipo from skin as sk join resultado_skin as rs on idSkin = fk_skin
  join usuario as us on fk_usuario = idUsuario where fk_usuario = 1;
  
--- drop database projeto_valorant;
